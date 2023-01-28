5fffc9a58cfa1118f968db7d2d11d1993112c2cb
SkMdSufiyan <107339274+SkMdSufiyan@users.noreply.github.com>
2023-01-28 07:58:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
   return (
     <div className='App'>
       <h6>Day 27 task (product CRUD with formik) </h6>
+  
+  <p className="netlify-suggestion-p">Kindly <b>DO NOT REFRESH</b> this netlify site when you are in the page other than home (all-products-list) page. If you refresh on page other than home page, then it will give "page not found" (problem with netlify deployment). </p>
+    
       <BrowserRouter>       
           <Routes>
 
